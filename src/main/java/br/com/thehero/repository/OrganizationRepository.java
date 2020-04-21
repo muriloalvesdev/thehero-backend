c75b1c0c123ce89f6ba698391c8d696f9ffb10c6
muriloalvesdev <murilo.alves@idtrust.com.br>
2020-04-21 16:46:20 -0300

    adicionando busca por cnpj

--- src/main/java/br/com/thehero/repository/OrganizationRepository.java
+++ src/main/java/br/com/thehero/repository/OrganizationRepository.java
@@ -1,5 +1,6 @@
 package br.com.thehero.repository;
 
+import java.util.Optional;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -7,5 +8,7 @@ import br.com.thehero.model.Organization;
 
 @Repository
 public interface OrganizationRepository extends JpaRepository<Organization, UUID> {
+
+  Optional<Organization> findByCnpj(String cnpj);
 
 }
