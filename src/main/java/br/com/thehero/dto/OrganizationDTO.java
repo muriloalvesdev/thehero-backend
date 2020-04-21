5c2a4d895ad4ba1bd87adde5ce0eba49302dee7c
muriloalvesdev <murilo.alves@idtrust.com.br>
2020-04-21 16:46:42 -0300

    removendo lombok das organizations

--- src/main/java/br/com/thehero/dto/OrganizationDTO.java
+++ src/main/java/br/com/thehero/dto/OrganizationDTO.java
@@ -1,21 +1,22 @@
 package br.com.thehero.dto;
 
+import javax.validation.constraints.Email;
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.br.CNPJ;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.Getter;
-import lombok.Setter;
 
-@Getter
-@Setter
 public class OrganizationDTO {
 
   @JsonProperty("name")
   private String name;
 
+  @Email(message = "email is not valid!")
   @JsonProperty("email")
   private String email;
 
+  @Length(max = 11, min = 10, message = "whatsapp is not valid!")
   @JsonProperty("whatsapp")
-  private String whatsapp;
+  private Integer whatsapp;
 
   @JsonProperty("city")
   private String city;
@@ -24,6 +25,66 @@ public class OrganizationDTO {
   private String uf;
 
   @JsonProperty("cnpj")
+  @CNPJ(message = "cnpj is not valid!")
   private String cnpj;
+
+  public OrganizationDTO(String name, @Email(message = "email is not valid!") String email,
+      @Length(max = 11, min = 10, message = "whatsapp is not valid!") Integer whatsapp, String city,
+      String uf, @CNPJ(message = "cnpj is not valid!") String cnpj) {
+    this.name = name;
+    this.email = email;
+    this.whatsapp = whatsapp;
+    this.city = city;
+    this.uf = uf;
+    this.cnpj = cnpj;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public Integer getWhatsapp() {
+    return whatsapp;
+  }
+
+  public void setWhatsapp(Integer whatsapp) {
+    this.whatsapp = whatsapp;
+  }
+
+  public String getCity() {
+    return city;
+  }
+
+  public void setCity(String city) {
+    this.city = city;
+  }
+
+  public String getUf() {
+    return uf;
+  }
+
+  public void setUf(String uf) {
+    this.uf = uf;
+  }
+
+  public String getCnpj() {
+    return cnpj;
+  }
+
+  public void setCnpj(String cnpj) {
+    this.cnpj = cnpj;
+  }
 
 }
--- src/main/java/br/com/thehero/model/Organization.java
+++ src/main/java/br/com/thehero/model/Organization.java
@@ -11,15 +11,10 @@ import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.persistence.UniqueConstraint;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
 
 @Entity
-@Table(name = "organization", uniqueConstraints = {@UniqueConstraint(columnNames = {"email"})})
-@Getter
-@Setter
-@AllArgsConstructor
+@Table(name = "organization",
+    uniqueConstraints = {@UniqueConstraint(columnNames = {"email", "cnpj"})})
 public class Organization {
 
   @GeneratedValue(strategy = GenerationType.AUTO)
@@ -40,11 +35,84 @@ public class Organization {
 
   @Column
   private String uf;
-  
+
   @Column
   private String cnpj;
 
   @OneToMany(mappedBy = "organization", fetch = FetchType.EAGER, orphanRemoval = true)
   private List<Incidents> incidents;
+
+  @SuppressWarnings("unused")
+  private Organization() {}
+
+  public Organization(String name, String email, String whatsapp, String city, String uf,
+      String cnpj) {
+    this.name = name;
+    this.email = email;
+    this.whatsapp = whatsapp;
+    this.city = city;
+    this.uf = uf;
+    this.cnpj = cnpj;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public String getWhatsapp() {
+    return whatsapp;
+  }
+
+  public void setWhatsapp(String whatsapp) {
+    this.whatsapp = whatsapp;
+  }
+
+  public String getCity() {
+    return city;
+  }
+
+  public void setCity(String city) {
+    this.city = city;
+  }
+
+  public String getUf() {
+    return uf;
+  }
+
+  public void setUf(String uf) {
+    this.uf = uf;
+  }
+
+  public String getCnpj() {
+    return cnpj;
+  }
+
+  public void setCnpj(String cnpj) {
+    this.cnpj = cnpj;
+  }
+
+  public List<Incidents> getIncidents() {
+    return incidents;
+  }
+
+  public void setIncidents(List<Incidents> incidents) {
+    this.incidents = incidents;
+  }
+
+  public UUID getUuid() {
+    return uuid;
+  }
 
 }
