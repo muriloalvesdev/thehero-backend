cb7e4e113e4fdfe4605d97b8bc8fa347d8d4fb4c
muriloalvesdev <murilo.alves@idtrust.com.br>
2020-04-21 19:03:37 -0300

    criando profile

--- src/main/java/br/com/thehero/repository/IncidentsRepository.java
+++ src/main/java/br/com/thehero/repository/IncidentsRepository.java
@@ -1,11 +1,15 @@
 package br.com.thehero.repository;
 
+import java.util.List;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import br.com.thehero.model.Incidents;
+import br.com.thehero.model.Organization;
 
 @Repository
 public interface IncidentsRepository extends JpaRepository<Incidents, UUID> {
+
+  List<Incidents> findByOrganization(Organization organization);
 
 }
