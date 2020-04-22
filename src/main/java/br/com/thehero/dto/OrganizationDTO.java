b7cfe592df61296aacf490457d8c404d9c52277e
muriloalvesdev <murilo.alves@idtrust.com.br>
2020-04-22 17:22:57 -0300

    os campos do dto ongs nao pode ser nulo

--- src/main/java/br/com/thehero/dto/OrganizationDTO.java
+++ src/main/java/br/com/thehero/dto/OrganizationDTO.java
@@ -1,29 +1,36 @@
 package br.com.thehero.dto;
 
 import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
 import org.hibernate.validator.constraints.br.CNPJ;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class OrganizationDTO {
 
   @JsonProperty("name")
+  @NotNull(message = "name is not valid!")
   private String name;
 
   @Email(message = "email is not valid!")
+  @NotNull(message = "email is not valid!")
   @JsonProperty("email")
   private String email;
 
   @JsonProperty("whatsapp")
+  @NotNull(message = "whatsapp is not valid!")
   private String whatsapp;
 
   @JsonProperty("city")
+  @NotNull(message = "city is not valid!")
   private String city;
 
   @JsonProperty("uf")
+  @NotNull(message = "uf is not valid!")
   private String uf;
 
   @JsonProperty("cnpj")
   @CNPJ(message = "cnpj is not valid!")
+  @NotNull(message = "cnpj is not valid!")
   private String cnpj;
 
   public OrganizationDTO(String name, @Email(message = "email is not valid!") String email,
