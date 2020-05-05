38c611f9a805f0fd129fd6968cc66deb0ff1a184
muriloalvesdev <murilo.alves@idtrust.com.br>
2020-05-05 15:24:53 -0300

    removendo anotação da interface, não é necessário em interfaces

--- src/main/java/br/com/thehero/domain/repository/IncidentsRepository.java
+++ src/main/java/br/com/thehero/domain/repository/IncidentsRepository.java
@@ -3,11 +3,9 @@ package br.com.thehero.domain.repository;
 import java.util.List;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 import br.com.thehero.domain.model.Incidents;
 import br.com.thehero.domain.model.Organization;
 
-@Repository
 public interface IncidentsRepository extends JpaRepository<Incidents, UUID> {
 
   List<Incidents> findByOrganization(Organization organization);
--- src/main/java/br/com/thehero/domain/repository/OrganizationRepository.java
+++ src/main/java/br/com/thehero/domain/repository/OrganizationRepository.java
@@ -3,10 +3,8 @@ package br.com.thehero.domain.repository;
 import java.util.Optional;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 import br.com.thehero.domain.model.Organization;
 
-@Repository
 public interface OrganizationRepository extends JpaRepository<Organization, UUID> {
 
   Optional<Organization> findByCnpj(String cnpj);
