e2824c424a292061c342c249fff55dfb36b88d82
muriloalvesdev <murilo.alves@idtrust.com.br>
2020-05-25 22:12:59 -0300

    add file in body request

--- src/main/java/br/com/thehero/controller/file/FileController.java
+++ src/main/java/br/com/thehero/controller/file/FileController.java
@@ -3,25 +3,24 @@ package br.com.thehero.controller.file;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
-
 import br.com.thehero.service.file.FilesService;
 
 @CrossOrigin(origins = "*")
 @RestController
 public class FileController {
 
-	FilesService service;
+  FilesService service;
 
-	public FileController(FilesService service) {
-		this.service = service;
-	}
+  public FileController(FilesService service) {
+    this.service = service;
+  }
 
-	@PostMapping("files/{cnpj}")
-	public void save(@RequestParam(name = "file") MultipartFile file,
-			@PathVariable(name = "cnpj") String cnpjOrganization) {
-		service.save(file, cnpjOrganization);
-	}
+  @PostMapping("files/{cnpj}")
+  public void save(@RequestBody MultipartFile file,
+      @PathVariable(name = "cnpj") String cnpjOrganization) {
+    service.save(file, cnpjOrganization);
+  }
 }
