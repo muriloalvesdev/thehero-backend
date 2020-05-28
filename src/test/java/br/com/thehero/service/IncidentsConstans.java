3dda5bc40be4427ce5f381753d6fc63b9bf5ec26
muriloalvesdev <murilo.alves@idtrust.com.br>
2020-05-28 17:59:12 -0300

    constantes para testes

---
+++ src/test/java/br/com/thehero/service/IncidentsConstans.java
@@ -0,0 +1,17 @@
+package br.com.thehero.service;
+
+/**
+ * 
+ * Constantes para testes, o objetivo é não precisar 
+ * reescrever todos os dados de um novo Incidente/Caso.
+ * 
+ * @author muriloalves 
+ * 
+ * @version 1.0
+ */
+public interface IncidentsConstans {
+  static final String TITLE = "Cão atropelado";
+  static final String DESCRIPTION =
+      "Cão encontrado na BR-365 está precisando de cuidados pós operatórios, nós conseguimos pagar a cirurgia veterinária, mas precisamos de ajuda com o pós operatório.";
+  static final String VALUE = "100";
+}
