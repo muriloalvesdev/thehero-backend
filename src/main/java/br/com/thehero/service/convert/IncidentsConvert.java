7612a719d058159a26a7549ad0950e5cd67b8111
muriloalvesdev <murilo.alves@idtrust.com.br>
2020-05-28 17:59:00 -0300

    Impl pattern builder nos incidents

--- src/main/java/br/com/thehero/service/convert/IncidentsConvert.java
+++ src/main/java/br/com/thehero/service/convert/IncidentsConvert.java
@@ -1,6 +1,7 @@
 package br.com.thehero.service.convert;
 
 import br.com.thehero.domain.model.Incidents;
+import br.com.thehero.domain.model.Incidents.IncidentsBuilder;
 import br.com.thehero.domain.model.Organization;
 import br.com.thehero.dto.IncidentsDTO;
 
@@ -10,13 +11,14 @@ public final class IncidentsConvert {
 
   public static final Incidents convertDataTransferObjetToEntity(IncidentsDTO dto,
       Organization organization) {
-    return new Incidents(dto.getTitle(), dto.getDescription(), dto.getValue(), organization);
+    return IncidentsBuilder.newBuilder(organization).withDescription(dto.getDescription())
+        .withTitle(dto.getTitle()).withValue(dto.getValue()).build();
   }
 
   public static final IncidentsDTO convertEntityToDataTransferObject(Incidents incidents) {
     Organization organization = incidents.getOrganization();
     return new IncidentsDTO(incidents.getTitle(), incidents.getDescription(), incidents.getValue(),
-        incidents.getUuid().toString(), organization.getName(), organization.getCity(),
+        incidents.getUuid().toString(), organization.getFullName(), organization.getCity(),
         organization.getUf(), organization.getWhatsapp(), organization.getEmail());
   }
 }
