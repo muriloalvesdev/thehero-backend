ecfa5c3fb0c6e6c30164e2d69bf74f35bddaa4c1
muriloalvesdev <murilo.alves@idtrust.com.br>
2020-06-16 21:49:17 -0300

    refatorando forma de salvar o arquivo e vinculando o mesmo ao caso

--- src/main/java/br/com/thehero/service/file/FilesService.java
+++ src/main/java/br/com/thehero/service/file/FilesService.java
@@ -2,6 +2,8 @@ package br.com.thehero.service.file;
 
 import org.springframework.web.multipart.MultipartFile;
 
+import br.com.thehero.domain.model.Files;
+
 public interface FilesService {
-  void save(MultipartFile files, String cnpjOrganization);
+	Files save(MultipartFile files, String cnpjOrganization);
 }
