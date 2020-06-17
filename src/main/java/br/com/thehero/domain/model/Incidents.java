1068b28ec59cfba823b2ebee55447352e0d3c793
muriloalvesdev <murilo.alves@idtrust.com.br>
2020-06-16 21:44:34 -0300

    adicionando relacionamento entre o caso e o arquivo

--- src/main/java/br/com/thehero/domain/model/Incidents.java
+++ src/main/java/br/com/thehero/domain/model/Incidents.java
@@ -1,6 +1,8 @@
 package br.com.thehero.domain.model;
 
 import java.util.UUID;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
@@ -10,76 +12,89 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "incidents")
 public class Incidents extends BaseEntity {
 
-  private static final long serialVersionUID = -7504356832234307582L;
+	private static final long serialVersionUID = -7504356832234307582L;
 
-  @Id
-  @GeneratedValue(strategy = GenerationType.AUTO)
-  private UUID uuid;
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private UUID uuid;
 
-  @Column
-  private String title;
+	@Column
+	private String title;
 
-  @Column
-  private String description;
+	@Column
+	private String description;
 
-  @Column
-  private String value;
+	@Column
+	private String value;
 
-  @ManyToOne(fetch = FetchType.EAGER)
-  @JoinColumn(name = "organization_uuid", referencedColumnName = "uuid",
-      foreignKey = @ForeignKey(name = "uuid"))
-  private Organization organization;
+	@ManyToOne(fetch = FetchType.EAGER)
+	@JoinColumn(name = "organization_uuid", referencedColumnName = "uuid", foreignKey = @ForeignKey(name = "uuid"))
+	private Organization organization;
 
-  @SuppressWarnings("unused")
-  private Incidents() {}
+	@OneToOne(fetch = FetchType.EAGER, cascade = CascadeType.REMOVE)
+	@JoinColumn(name = "files_uuid", referencedColumnName = "uuid", foreignKey = @ForeignKey(name = "uuid"))
+	private Files files;
 
-  public Incidents(String title, String description, String value, Organization organization) {
-    this.title = title;
-    this.description = description;
-    this.value = value;
-    this.organization = organization;
-  }
+	@SuppressWarnings("unused")
+	private Incidents() {
+	}
 
-  public String getTitle() {
-    return title;
-  }
+	public Incidents(String title, String description, String value, Organization organization) {
+		this.title = title;
+		this.description = description;
+		this.value = value;
+		this.organization = organization;
+	}
 
-  public void setTitle(String title) {
-    this.title = title;
-  }
+	public String getTitle() {
+		return title;
+	}
 
-  public String getDescription() {
-    return description;
-  }
+	public Files getFiles() {
+		return files;
+	}
 
-  public void setDescription(String description) {
-    this.description = description;
-  }
+	public void setFiles(Files files) {
+		this.files = files;
+	}
 
-  public String getValue() {
-    return value;
-  }
+	public void setTitle(String title) {
+		this.title = title;
+	}
 
-  public void setValue(String value) {
-    this.value = value;
-  }
+	public String getDescription() {
+		return description;
+	}
 
-  public Organization getOrganization() {
-    return organization;
-  }
+	public void setDescription(String description) {
+		this.description = description;
+	}
 
-  public void setOrganization(Organization organization) {
-    this.organization = organization;
-  }
+	public String getValue() {
+		return value;
+	}
 
-  public UUID getUuid() {
-    return uuid;
-  }
+	public void setValue(String value) {
+		this.value = value;
+	}
+
+	public Organization getOrganization() {
+		return organization;
+	}
+
+	public void setOrganization(Organization organization) {
+		this.organization = organization;
+	}
+
+	public UUID getUuid() {
+		return uuid;
+	}
 
 }
