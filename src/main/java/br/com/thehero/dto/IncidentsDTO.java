bae6d6590e15f5616a76dc891fd54e3923c6fb7e
muriloalvesdev <murilo.alves@idtrust.com.br>
2020-06-16 21:46:52 -0300

    no DTO do caso deve conter o tipo do arquivo e o arquivo em byte[]

--- src/main/java/br/com/thehero/dto/IncidentsDTO.java
+++ src/main/java/br/com/thehero/dto/IncidentsDTO.java
@@ -1,125 +1,152 @@
 package br.com.thehero.dto;
 
 import javax.validation.constraints.NotNull;
+
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class IncidentsDTO {
 
-  @JsonProperty("id")
-  private String id;
+	@JsonProperty("id")
+	private String id;
 
-  @JsonProperty("title")
-  @NotNull
-  private String title;
+	@JsonProperty("title")
+	@NotNull
+	private String title;
 
-  @JsonProperty("description")
-  @NotNull
-  private String description;
+	@JsonProperty("description")
+	@NotNull
+	private String description;
 
-  @JsonProperty("value")
-  @NotNull
-  private String value;
+	@JsonProperty("value")
+	@NotNull
+	private String value;
 
-  @JsonProperty("name")
-  private String nameOrganization;
+	@JsonProperty("name")
+	private String nameOrganization;
 
-  @JsonProperty("city")
-  private String city;
+	@JsonProperty("city")
+	private String city;
 
-  @JsonProperty("uf")
-  private String uf;
+	@JsonProperty("uf")
+	private String uf;
 
-  @JsonProperty("whatsapp")
-  private String whatsapp;
+	@JsonProperty("whatsapp")
+	private String whatsapp;
 
-  @JsonProperty("email")
-  private String email;
+	@JsonProperty("email")
+	private String email;
 
-  public IncidentsDTO(@NotNull String title, @NotNull String description, @NotNull String value,
-      String id, String nameOrganization, String city, String uf, String whatsapp, String email) {
-    this.title = title;
-    this.description = description;
-    this.value = value;
-    this.id = id;
-    this.nameOrganization = nameOrganization;
-    this.city = city;
-    this.uf = uf;
-    this.whatsapp = whatsapp;
-    this.email = email;
-  }
+	@JsonProperty("file_data")
+	private byte[] fileData;
 
-  public IncidentsDTO() {}
+	@JsonProperty("mime_type")
+	private String mimeType;
 
-  public String getWhatsapp() {
-    return whatsapp;
-  }
+	public IncidentsDTO(@NotNull String title, @NotNull String description, @NotNull String value, String id,
+			String nameOrganization, String city, String uf, String whatsapp, String email, byte[] fileData,
+			String mimeType) {
+		this.title = title;
+		this.description = description;
+		this.value = value;
+		this.id = id;
+		this.nameOrganization = nameOrganization;
+		this.city = city;
+		this.uf = uf;
+		this.whatsapp = whatsapp;
+		this.email = email;
+		this.fileData = fileData;
+		this.mimeType = mimeType;
+	}
 
-  public void setWhatsapp(String whatsapp) {
-    this.whatsapp = whatsapp;
-  }
+	public IncidentsDTO() {
+	}
 
-  public String getEmail() {
-    return email;
-  }
+	public String getMimeType() {
+		return mimeType;
+	}
 
-  public void setEmail(String email) {
-    this.email = email;
-  }
+	public void setMimeType(String mimeType) {
+		this.mimeType = mimeType;
+	}
 
+	public byte[] getFileData() {
+		return fileData;
+	}
 
-  public String getCity() {
-    return city;
-  }
+	public void setFileData(byte[] fileData) {
+		this.fileData = fileData;
+	}
 
-  public void setCity(String city) {
-    this.city = city;
-  }
+	public String getWhatsapp() {
+		return whatsapp;
+	}
 
-  public String getUf() {
-    return uf;
-  }
+	public void setWhatsapp(String whatsapp) {
+		this.whatsapp = whatsapp;
+	}
 
-  public void setUf(String uf) {
-    this.uf = uf;
-  }
+	public String getEmail() {
+		return email;
+	}
 
-  public String getNameOrganization() {
-    return nameOrganization;
-  }
+	public void setEmail(String email) {
+		this.email = email;
+	}
 
-  public void setNameOrganization(String nameOrganization) {
-    this.nameOrganization = nameOrganization;
-  }
+	public String getCity() {
+		return city;
+	}
 
-  public String getId() {
-    return id;
-  }
+	public void setCity(String city) {
+		this.city = city;
+	}
 
-  public void setId(String id) {
-    this.id = id;
-  }
+	public String getUf() {
+		return uf;
+	}
 
-  public String getTitle() {
-    return title;
-  }
+	public void setUf(String uf) {
+		this.uf = uf;
+	}
 
-  public void setTitle(String title) {
-    this.title = title;
-  }
+	public String getNameOrganization() {
+		return nameOrganization;
+	}
 
-  public String getDescription() {
-    return description;
-  }
+	public void setNameOrganization(String nameOrganization) {
+		this.nameOrganization = nameOrganization;
+	}
 
-  public void setDescription(String description) {
-    this.description = description;
-  }
+	public String getId() {
+		return id;
+	}
 
-  public String getValue() {
-    return value;
-  }
+	public void setId(String id) {
+		this.id = id;
+	}
 
-  public void setValue(String value) {
-    this.value = value;
-  }
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getValue() {
+		return value;
+	}
+
+	public void setValue(String value) {
+		this.value = value;
+	}
+
 }
