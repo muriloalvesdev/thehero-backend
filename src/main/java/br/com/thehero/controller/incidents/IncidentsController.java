218d62828977067b50d7a203ced24c0e265d2dfc
muriloalvesdev <murilo.alves@idtrust.com.br>
2020-06-16 21:48:14 -0300

    adicionando no retorno da api de criacao do caso o retorno do id do caso

--- src/main/java/br/com/thehero/controller/incidents/IncidentsController.java
+++ src/main/java/br/com/thehero/controller/incidents/IncidentsController.java
@@ -2,6 +2,7 @@ package br.com.thehero.controller.incidents;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.annotation.Validated;
@@ -13,7 +14,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.com.thehero.domain.model.Incidents;
 import br.com.thehero.dto.IncidentsDTO;
@@ -25,41 +25,39 @@ import javassist.NotFoundException;
 @RestController
 public class IncidentsController {
 
-  private IncidentsService service;
+	private IncidentsService service;
 
-  public IncidentsController(IncidentsService service) {
-    this.service = service;
-  }
+	public IncidentsController(IncidentsService service) {
+		this.service = service;
+	}
 
-  @DeleteMapping("incidents/{id}/{cnpj}")
-  @PreAuthorize("hasRole('ADMIN')")
-  public ResponseEntity<Object> delete(
-      @PathVariable(name = "id", required = true) String incidentsId,
-      @PathVariable(value = "cnpj", required = true) String cnpjOrganization) {
-    service.delete(incidentsId, cnpjOrganization);
-    return ResponseEntity.noContent().build();
-  }
+	@DeleteMapping("incidents/{id}/{cnpj}")
+	@PreAuthorize("hasRole('ADMIN')")
+	public ResponseEntity<Object> delete(@PathVariable(name = "id", required = true) String incidentsId,
+			@PathVariable(value = "cnpj", required = true) String cnpjOrganization) {
+		service.delete(incidentsId, cnpjOrganization);
+		return ResponseEntity.noContent().build();
+	}
 
-  @GetMapping("incidents/{id}")
-  @PreAuthorize("hasRole('ADMIN')")
-  public ResponseEntity<IncidentsDTO> findById(
-      @PathVariable(name = "id", required = true) String incidentsId) throws NotFoundException {
-    return ResponseEntity.ok(service.findById(incidentsId));
-  }
+	@GetMapping("incidents/{id}")
+	@PreAuthorize("hasRole('ADMIN')")
+	public ResponseEntity<IncidentsDTO> findById(@PathVariable(name = "id", required = true) String incidentsId)
+			throws NotFoundException {
+		return ResponseEntity.ok(service.findById(incidentsId));
+	}
 
-  @PostMapping("incidents/{cnpj}")
-  @PreAuthorize("hasRole('ADMIN')")
-  public ResponseEntity<Object> create(@Validated @RequestBody IncidentsDTO dto,
-      @PathVariable(name = "cnpj", required = true) String cnpjOrganization) {
-    Incidents incidents = service.create(dto, cnpjOrganization);
-    return ResponseEntity.created(ServletUriComponentsBuilder.fromCurrentContextPath()
-        .path("/incidents/{id}").buildAndExpand(incidents.getUuid()).toUri()).build();
-  }
+	@PostMapping("incidents/{cnpj}")
+	@PreAuthorize("hasRole('ADMIN')")
+	public ResponseEntity<Object> create(@Validated @RequestBody IncidentsDTO dto,
+			@PathVariable(name = "cnpj", required = true) String cnpjOrganization) {
+		Incidents incidents = service.create(dto, cnpjOrganization);
+		return new ResponseEntity<>(incidents.getUuid().toString(), HttpStatus.CREATED);
+	}
 
-  @GetMapping("incidents")
-  @PreAuthorize("hasRole('ADMIN')")
-  public ResponseEntity<Page<IncidentsDTO>> findAll(Pageable pageable) {
-    return ResponseEntity.ok(service.findAll(pageable));
-  }
+	@GetMapping("incidents")
+	@PreAuthorize("hasRole('ADMIN')")
+	public ResponseEntity<Page<IncidentsDTO>> findAll(Pageable pageable) {
+		return ResponseEntity.ok(service.findAll(pageable));
+	}
 
 }
