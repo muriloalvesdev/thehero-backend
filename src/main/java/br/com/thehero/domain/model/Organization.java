776250364c5c95a8e7755bc48b553c4f92b2d911
muriloalvesdev <murilo.alves@idtrust.com.br>
2020-06-16 21:44:16 -0300

    removendo relacao da organizacao com os arquivos

--- src/main/java/br/com/thehero/domain/model/Organization.java
+++ src/main/java/br/com/thehero/domain/model/Organization.java
@@ -2,6 +2,7 @@ package br.com.thehero.domain.model;
 
 import java.util.List;
 import java.util.UUID;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
@@ -12,167 +13,156 @@ import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.persistence.UniqueConstraint;
 import javax.validation.constraints.Email;
+
 import com.sun.istack.NotNull;
 
 @Entity
-@Table(name = "organization",
-    uniqueConstraints = {@UniqueConstraint(columnNames = {"email", "cnpj"})})
+@Table(name = "organization", uniqueConstraints = { @UniqueConstraint(columnNames = { "email", "cnpj" }) })
 public class Organization extends BaseEntity {
 
-  private static final long serialVersionUID = 6997726546986427669L;
+	private static final long serialVersionUID = 6997726546986427669L;
 
-  @GeneratedValue(strategy = GenerationType.AUTO)
-  @Id
-  private UUID uuid;
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Id
+	private UUID uuid;
 
-  @Column
-  private String name;
+	@Column
+	private String name;
 
-  @Column
-  private String email;
+	@Column
+	private String email;
 
-  @Column
-  private String whatsapp;
+	@Column
+	private String whatsapp;
 
-  @Column
-  private String city;
+	@Column
+	private String city;
 
-  @Column
-  private String uf;
+	@Column
+	private String uf;
 
-  @Column
-  private String cnpj;
+	@Column
+	private String cnpj;
 
-  @OneToMany(mappedBy = "organization", fetch = FetchType.LAZY, orphanRemoval = true)
-  private List<Incidents> incidents;
+	@OneToMany(mappedBy = "organization", fetch = FetchType.LAZY, orphanRemoval = true)
+	private List<Incidents> incidents;
 
-  @OneToMany(mappedBy = "organization", fetch = FetchType.EAGER, orphanRemoval = true)
-  private List<Files> files;
+	private Organization() {
+	}
 
-  private Organization() {}
+	private Organization(String name, String email, String whatsapp, String city, String uf, String cnpj) {
+		this.name = name;
+		this.email = email;
+		this.whatsapp = whatsapp;
+		this.city = city;
+		this.uf = uf;
+		this.cnpj = cnpj;
+	}
 
-  private Organization(String name, String email, String whatsapp, String city, String uf,
-      String cnpj) {
-    this.name = name;
-    this.email = email;
-    this.whatsapp = whatsapp;
-    this.city = city;
-    this.uf = uf;
-    this.cnpj = cnpj;
-  }
+	public String getName() {
+		return name;
+	}
 
-  public List<Files> getFiles() {
-    return files;
-  }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-  public void setFiles(List<Files> files) {
-    this.files = files;
-  }
+	public String getEmail() {
+		return email;
+	}
 
-  public String getName() {
-    return name;
-  }
+	public void setEmail(String email) {
+		this.email = email;
+	}
 
-  public void setName(String name) {
-    this.name = name;
-  }
+	public String getWhatsapp() {
+		return whatsapp;
+	}
 
-  public String getEmail() {
-    return email;
-  }
+	public void setWhatsapp(String whatsapp) {
+		this.whatsapp = whatsapp;
+	}
 
-  public void setEmail(String email) {
-    this.email = email;
-  }
+	public String getCity() {
+		return city;
+	}
 
-  public String getWhatsapp() {
-    return whatsapp;
-  }
+	public void setCity(String city) {
+		this.city = city;
+	}
 
-  public void setWhatsapp(String whatsapp) {
-    this.whatsapp = whatsapp;
-  }
+	public String getUf() {
+		return uf;
+	}
 
-  public String getCity() {
-    return city;
-  }
+	public void setUf(String uf) {
+		this.uf = uf;
+	}
 
-  public void setCity(String city) {
-    this.city = city;
-  }
+	public String getCnpj() {
+		return cnpj;
+	}
 
-  public String getUf() {
-    return uf;
-  }
+	public void setCnpj(String cnpj) {
+		this.cnpj = cnpj;
+	}
 
-  public void setUf(String uf) {
-    this.uf = uf;
-  }
+	public List<Incidents> getIncidents() {
+		return incidents;
+	}
 
-  public String getCnpj() {
-    return cnpj;
-  }
+	public void setIncidents(List<Incidents> incidents) {
+		this.incidents = incidents;
+	}
 
-  public void setCnpj(String cnpj) {
-    this.cnpj = cnpj;
-  }
+	public UUID getUuid() {
+		return uuid;
+	}
 
-  public List<Incidents> getIncidents() {
-    return incidents;
-  }
+	public static class OrganizationBuilder {
+		private String name;
+		private String email;
+		private String whatsapp;
+		private String city;
+		private String uf;
+		private String cnpj;
 
-  public void setIncidents(List<Incidents> incidents) {
-    this.incidents = incidents;
-  }
+		public static OrganizationBuilder newBuilder(@NotNull String name) {
+			return new OrganizationBuilder(name);
+		}
 
-  public UUID getUuid() {
-    return uuid;
-  }
+		private OrganizationBuilder(@NotNull String name) {
+			this.name = name;
+		}
 
-  public static class OrganizationBuilder {
-    private String name;
-    private String email;
-    private String whatsapp;
-    private String city;
-    private String uf;
-    private String cnpj;
+		public OrganizationBuilder withEmail(@Email String email) {
+			this.email = email;
+			return this;
+		}
 
-    public static OrganizationBuilder newBuilder(@NotNull String name) {
-      return new OrganizationBuilder(name);
-    }
+		public OrganizationBuilder withWhatsapp(@NotNull String whatsapp) {
+			this.whatsapp = whatsapp;
+			return this;
+		}
 
-    private OrganizationBuilder(@NotNull String name) {
-      this.name = name;
-    }
+		public OrganizationBuilder withCity(@NotNull String city) {
+			this.city = city;
+			return this;
+		}
 
-    public OrganizationBuilder withEmail(@Email String email) {
-      this.email = email;
-      return this;
-    }
+		public OrganizationBuilder withUf(@NotNull String uf) {
+			this.uf = uf;
+			return this;
+		}
 
-    public OrganizationBuilder withWhatsapp(@NotNull String whatsapp) {
-      this.whatsapp = whatsapp;
-      return this;
-    }
+		public OrganizationBuilder withCnpj(@NotNull String cnpj) {
+			this.cnpj = cnpj;
+			return this;
+		}
 
-    public OrganizationBuilder withCity(@NotNull String city) {
-      this.city = city;
-      return this;
-    }
-
-    public OrganizationBuilder withUf(@NotNull String uf) {
-      this.uf = uf;
-      return this;
-    }
-
-    public OrganizationBuilder withCnpj(@NotNull String cnpj) {
-      this.cnpj = cnpj;
-      return this;
-    }
-
-    public Organization build() {
-      return new Organization(name, email, whatsapp, city, uf, cnpj);
-    }
-  }
+		public Organization build() {
+			return new Organization(name, email, whatsapp, city, uf, cnpj);
+		}
+	}
 
 }
