a49d801d9a842498d125f89b11889a23e652cfa8
JonatasMacedo <jonatasfernandoaraujomacedo@gmail.com>
2020-06-17 22:53:48 -0300

    Removendo mapeamento de verbo HTTP para DELETE

--- src/main/java/br/com/thehero/login/config/WebConfig.java
+++ src/main/java/br/com/thehero/login/config/WebConfig.java
@@ -10,7 +10,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
   @Override
   public void addCorsMappings(CorsRegistry registry) {
-    registry.addMapping("/api/**").allowedOrigins("*").allowedMethods("POST,PUT,GET,DELETE,OPTIONS")
+    registry.addMapping("/api/**").allowedOrigins("*").allowedMethods("POST,PUT,GET,OPTIONS")
         .allowedHeaders("Location", "Content-type", "authorization")
         .exposedHeaders("Location", "Content-Disposition");
   }
