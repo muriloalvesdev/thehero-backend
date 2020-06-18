a64071b08e90fd024c4d7a668e4de7fc3ab5fb7f
JonatasMacedo <jonatasfernandoaraujomacedo@gmail.com>
2020-06-17 22:57:03 -0300

    removendo [delete]

--- src/main/java/br/com/thehero/controller/organization/OrganizationController.java
+++ src/main/java/br/com/thehero/controller/organization/OrganizationController.java
@@ -1,12 +1,14 @@
 package br.com.thehero.controller.organization;
 
+import br.com.thehero.domain.model.Organization;
+import br.com.thehero.dto.OrganizationDTO;
+import br.com.thehero.service.organization.OrganizationService;
 import java.util.List;
-
+import javassist.NotFoundException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,11 +16,6 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-
-import br.com.thehero.domain.model.Organization;
-import br.com.thehero.dto.OrganizationDTO;
-import br.com.thehero.service.organization.OrganizationService;
-import javassist.NotFoundException;
 
 @CrossOrigin(origins = "*")
 @RestController
@@ -42,29 +39,25 @@ public class OrganizationController {
       @Validated @RequestBody OrganizationDTO organizationDTO) {
     Organization organization = service.create(organizationDTO);
 
-    return ResponseEntity.created(ServletUriComponentsBuilder.fromCurrentContextPath()
-        .path("/ongs/{cnpj}").buildAndExpand(organization.getCnpj()).toUri()).build();
+    return ResponseEntity.created(
+            ServletUriComponentsBuilder.fromCurrentContextPath()
+                .path("/ongs/{cnpj}")
+                .buildAndExpand(organization.getCnpj())
+                .toUri())
+        .build();
   }
 
   @GetMapping("/ongs/{cnpj}")
   @PreAuthorize("hasRole('ADMIN')")
-  public ResponseEntity<OrganizationDTO> findByCnpj(
-      @PathVariable(required = true, name = "cnpj") String cnpj) throws NotFoundException {
+  public ResponseEntity<OrganizationDTO> findByCnpj(@PathVariable(name = "cnpj") String cnpj)
+      throws NotFoundException {
     return ResponseEntity.ok(service.findByCnpj(cnpj));
   }
 
   @PreAuthorize("hasRole('ADMIN')")
   @PutMapping("/ongs")
-  public ResponseEntity<Object> update(@Validated @RequestBody OrganizationDTO organizationDTO) {
+  public ResponseEntity<OrganizationDTO> update(@Validated @RequestBody OrganizationDTO organizationDTO) {
     service.update(organizationDTO);
     return ResponseEntity.ok().build();
-  }
-
-  @PreAuthorize("hasRole('ADMIN')")
-  @DeleteMapping("/ongs/{cnpj}")
-  public ResponseEntity<Void> delete(@PathVariable(name = "cnpj") String cnpj)
-      throws NotFoundException {
-    service.delete(cnpj);
-    return ResponseEntity.noContent().build();
   }
 }
