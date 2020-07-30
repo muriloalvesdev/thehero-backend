8394a15d32aa58b31e71d03a980807af9c797d74
muriloalvesdev <murilo.alves@idtrust.com.br>
2020-07-30 18:40:36 -0300

    add checkstyle

--- src/main/java/br/com/thehero/domain/model/Organization.java
+++ src/main/java/br/com/thehero/domain/model/Organization.java
@@ -17,152 +17,153 @@ import javax.validation.constraints.Email;
 import com.sun.istack.NotNull;
 
 @Entity
-@Table(name = "organization", uniqueConstraints = { @UniqueConstraint(columnNames = { "email", "cnpj" }) })
+@Table(name = "organization",
+    uniqueConstraints = {@UniqueConstraint(columnNames = {"email", "cnpj"})})
 public class Organization extends BaseEntity {
 
-	private static final long serialVersionUID = 6997726546986427669L;
+  private static final long serialVersionUID = 6997726546986427669L;
 
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	@Id
-	private UUID uuid;
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  @Id
+  private UUID uuid;
 
-	@Column
-	private String name;
+  @Column
+  private String name;
 
-	@Column
-	private String email;
+  @Column
+  private String email;
 
-	@Column
-	private String whatsapp;
+  @Column
+  private String whatsapp;
 
-	@Column
-	private String city;
+  @Column
+  private String city;
 
-	@Column
-	private String uf;
+  @Column
+  private String uf;
 
-	@Column
-	private String cnpj;
+  @Column
+  private String cnpj;
 
-	@OneToMany(mappedBy = "organization", fetch = FetchType.LAZY, orphanRemoval = true)
-	private List<Incidents> incidents;
+  @OneToMany(mappedBy = "organization", fetch = FetchType.LAZY, orphanRemoval = true)
+  private List<Incidents> incidents;
 
-	private Organization() {
-	}
+  private Organization() {}
 
-	private Organization(String name, String email, String whatsapp, String city, String uf, String cnpj) {
-		this.name = name;
-		this.email = email;
-		this.whatsapp = whatsapp;
-		this.city = city;
-		this.uf = uf;
-		this.cnpj = cnpj;
-	}
+  private Organization(String name, String email, String whatsapp, String city, String uf,
+      String cnpj) {
+    this.name = name;
+    this.email = email;
+    this.whatsapp = whatsapp;
+    this.city = city;
+    this.uf = uf;
+    this.cnpj = cnpj;
+  }
 
-	public String getName() {
-		return name;
-	}
+  public String getName() {
+    return name;
+  }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+  public void setName(String name) {
+    this.name = name;
+  }
 
-	public String getEmail() {
-		return email;
-	}
+  public String getEmail() {
+    return email;
+  }
 
-	public void setEmail(String email) {
-		this.email = email;
-	}
+  public void setEmail(String email) {
+    this.email = email;
+  }
 
-	public String getWhatsapp() {
-		return whatsapp;
-	}
+  public String getWhatsapp() {
+    return whatsapp;
+  }
 
-	public void setWhatsapp(String whatsapp) {
-		this.whatsapp = whatsapp;
-	}
+  public void setWhatsapp(String whatsapp) {
+    this.whatsapp = whatsapp;
+  }
 
-	public String getCity() {
-		return city;
-	}
+  public String getCity() {
+    return city;
+  }
 
-	public void setCity(String city) {
-		this.city = city;
-	}
+  public void setCity(String city) {
+    this.city = city;
+  }
 
-	public String getUf() {
-		return uf;
-	}
+  public String getUf() {
+    return uf;
+  }
 
-	public void setUf(String uf) {
-		this.uf = uf;
-	}
+  public void setUf(String uf) {
+    this.uf = uf;
+  }
 
-	public String getCnpj() {
-		return cnpj;
-	}
+  public String getCnpj() {
+    return cnpj;
+  }
 
-	public void setCnpj(String cnpj) {
-		this.cnpj = cnpj;
-	}
+  public void setCnpj(String cnpj) {
+    this.cnpj = cnpj;
+  }
 
-	public List<Incidents> getIncidents() {
-		return incidents;
-	}
+  public List<Incidents> getIncidents() {
+    return incidents;
+  }
 
-	public void setIncidents(List<Incidents> incidents) {
-		this.incidents = incidents;
-	}
+  public void setIncidents(List<Incidents> incidents) {
+    this.incidents = incidents;
+  }
 
-	public UUID getUuid() {
-		return uuid;
-	}
+  public UUID getUuid() {
+    return uuid;
+  }
 
-	public static class OrganizationBuilder {
-		private String name;
-		private String email;
-		private String whatsapp;
-		private String city;
-		private String uf;
-		private String cnpj;
+  public static class OrganizationBuilder {
+    private String name;
+    private String email;
+    private String whatsapp;
+    private String city;
+    private String uf;
+    private String cnpj;
 
-		public static OrganizationBuilder newBuilder(@NotNull String name) {
-			return new OrganizationBuilder(name);
-		}
+    public static OrganizationBuilder newBuilder(@NotNull String name) {
+      return new OrganizationBuilder(name);
+    }
 
-		private OrganizationBuilder(@NotNull String name) {
-			this.name = name;
-		}
+    private OrganizationBuilder(@NotNull String name) {
+      this.name = name;
+    }
 
-		public OrganizationBuilder withEmail(@Email String email) {
-			this.email = email;
-			return this;
-		}
+    public OrganizationBuilder withEmail(@Email String email) {
+      this.email = email;
+      return this;
+    }
 
-		public OrganizationBuilder withWhatsapp(@NotNull String whatsapp) {
-			this.whatsapp = whatsapp;
-			return this;
-		}
+    public OrganizationBuilder withWhatsapp(@NotNull String whatsapp) {
+      this.whatsapp = whatsapp;
+      return this;
+    }
 
-		public OrganizationBuilder withCity(@NotNull String city) {
-			this.city = city;
-			return this;
-		}
+    public OrganizationBuilder withCity(@NotNull String city) {
+      this.city = city;
+      return this;
+    }
 
-		public OrganizationBuilder withUf(@NotNull String uf) {
-			this.uf = uf;
-			return this;
-		}
+    public OrganizationBuilder withUf(@NotNull String uf) {
+      this.uf = uf;
+      return this;
+    }
 
-		public OrganizationBuilder withCnpj(@NotNull String cnpj) {
-			this.cnpj = cnpj;
-			return this;
-		}
+    public OrganizationBuilder withCnpj(@NotNull String cnpj) {
+      this.cnpj = cnpj;
+      return this;
+    }
 
-		public Organization build() {
-			return new Organization(name, email, whatsapp, city, uf, cnpj);
-		}
-	}
+    public Organization build() {
+      return new Organization(name, email, whatsapp, city, uf, cnpj);
+    }
+  }
 
 }
--- src/main/java/br/com/thehero/service/file/impl/FilesServiceImpl.java
+++ src/main/java/br/com/thehero/service/file/impl/FilesServiceImpl.java
@@ -17,43 +17,48 @@ import br.com.thehero.service.file.FilesService;
 @Service
 public class FilesServiceImpl implements FilesService {
 
-	static final Logger LOG = Logger.getLogger(FilesServiceImpl.class);
-	FilesRepository filesRepository;
-	IncidentsRepository incidentsRepository;
+  static final Logger LOG = Logger.getLogger(FilesServiceImpl.class);
+  FilesRepository filesRepository;
+  IncidentsRepository incidentsRepository;
 
-	public FilesServiceImpl(FilesRepository filesRepository, IncidentsRepository incidentsRepository) {
-		this.filesRepository = filesRepository;
-		this.incidentsRepository = incidentsRepository;
-	}
+  public FilesServiceImpl(FilesRepository filesRepository,
+      IncidentsRepository incidentsRepository) {
+    this.filesRepository = filesRepository;
+    this.incidentsRepository = incidentsRepository;
+  }
 
-	public Files save(MultipartFile file, String uuidIncidents) {
-		try {
-			LOG.info("[BEGIN] save() - with filename [" + file.getName() + "]");
-			return uploadFile(file, uuidIncidents);
-		} catch (IOException e) {
-			LOG.error("Persist file error: " + e.getMessage(), e);
-		} finally {
-			LOG.info("[END] save()");
-		}
-		return null;
-	}
+  public Files save(MultipartFile file, String uuidIncidents) {
+    try {
+      LOG.info("[BEGIN] save() - with filename [" + file.getName() + "]");
+      return uploadFile(file, uuidIncidents);
+    } catch (IOException e) {
+      LOG.error("Persist file error: " + e.getMessage(), e);
+    } finally {
+      LOG.info("[END] save()");
+    }
+    return null;
+  }
 
-	private Files uploadFile(MultipartFile file, String uuidIncidents) throws IOException {
-		byte[] data = file.getBytes();
-		String filename = file.getOriginalFilename();
-		String type = file.getContentType();
+  private Files uploadFile(MultipartFile file, String uuidIncidents) throws IOException {
+    byte[] data = file.getBytes();
+    String type = file.getContentType();
+    String filename = file.getOriginalFilename();
+    validateFilename(filename);
 
-		Incidents incidents = incidentsRepository.findById(UUID.fromString(uuidIncidents)).orElseThrow(
-				() -> new RuntimeException("Incident not found with UUID informed [" + uuidIncidents + "]"));
+    Incidents incidents = incidentsRepository.findById(UUID.fromString(uuidIncidents))
+        .orElseThrow(() -> new RuntimeException(
+            "Incident not found with UUID informed [" + uuidIncidents + "]"));
 
-		if (filename.contains("..")) {
-			throw new RuntimeException("Sorry! Filename contains invalid path sequence " + filename);
-		}
+    Files files = filesRepository.saveAndFlush(FilesBuilder.newBuilder(data).withFilename(filename)
+        .withType(type).withIncidents(incidents).build());
+    incidents.setFiles(files);
+    incidentsRepository.saveAndFlush(incidents);
+    return files;
+  }
 
-		Files files = filesRepository.saveAndFlush(
-				FilesBuilder.newBuilder(data).withFilename(filename).withType(type).withIncidents(incidents).build());
-		incidents.setFiles(files);
-		incidentsRepository.saveAndFlush(incidents);
-		return files;
-	}
+  private void validateFilename(String filename) {
+    if (filename.contains("..")) {
+      throw new RuntimeException("Sorry! Filename contains invalid path sequence " + filename);
+    }
+  }
 }
