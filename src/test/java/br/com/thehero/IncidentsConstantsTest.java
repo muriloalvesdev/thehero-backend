320d32d57c3458515b0c44da82e2f3796adf26a2
muriloalvesdev <murilo.alves@idtrust.com.br>
2020-09-17 00:43:47 -0300

    constants for tests

---
+++ src/test/java/br/com/thehero/IncidentsConstantsTest.java
@@ -0,0 +1,9 @@
+package br.com.thehero;
+
+public interface IncidentsConstantsTest {
+  static final String TITLE = "Castração de cadela em estado crítico";
+  static final String VALUE = "100.00";
+  static final String DESCRIPTION =
+      "Precisamos de 100 reais para conseguir pagar a castração da Nina, por favor nos ajudem!";
+  static final String MIME_TYPE = ".png";
+}
