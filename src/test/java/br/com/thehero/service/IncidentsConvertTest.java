9349774abdf5793f1e69dc0887112932d9898809
muriloalvesdev <murilo.alves@idtrust.com.br>
2020-09-17 00:13:06 -0300

    checkstyle

--- src/test/java/br/com/thehero/service/IncidentsConvertTest.java
+++ src/test/java/br/com/thehero/service/IncidentsConvertTest.java
@@ -9,30 +9,29 @@ import org.junit.Test;
 
 public class IncidentsConvertTest {
 
-    @Test public void convertDataTransferObjetToEntityTest() {
-        Organization organization =
-            Organization.OrganizationBuilder.newBuilder("Anjos de Patas LTDA")
-                .withCity("Ribeirão Preto").withCnpj("59533195000188")
-                .withEmail("contato@anjosdepatas.com.br").withUf("SP").withWhatsapp("11999999999")
-                .build();
-        final byte[] teste = new byte[0];
-        IncidentsDTO incidentsDTO = new IncidentsDTO();
-        incidentsDTO.setCity("cidade");
-        incidentsDTO.setDescription("descricao");
-        incidentsDTO.setEmail("teste@teste.com");
-        incidentsDTO.setFileData(teste);
-        incidentsDTO.setId("2");
-        incidentsDTO.setMimeType(".png");
-        incidentsDTO.setNameOrganization("nome organizacao");
-        incidentsDTO.setTitle("titulo");
-        incidentsDTO.setUf("SP");
-        incidentsDTO.setWhatsapp("1111111111111");
+  @Test
+  public void convertDataTransferObjetToEntityTest() {
+    Organization organization = Organization.OrganizationBuilder.newBuilder("Anjos de Patas LTDA")
+        .withCity("Ribeirão Preto").withCnpj("59533195000188")
+        .withEmail("contato@anjosdepatas.com.br").withUf("SP").withWhatsapp("11999999999").build();
+    final byte[] teste = new byte[0];
+    IncidentsDTO incidentsDTO = new IncidentsDTO();
+    incidentsDTO.setCity("cidade");
+    incidentsDTO.setDescription("descricao");
+    incidentsDTO.setEmail("teste@teste.com");
+    incidentsDTO.setFileData(teste);
+    incidentsDTO.setId("2");
+    incidentsDTO.setMimeType(".png");
+    incidentsDTO.setNameOrganization("nome organizacao");
+    incidentsDTO.setTitle("titulo");
+    incidentsDTO.setUf("SP");
+    incidentsDTO.setWhatsapp("1111111111111");
 
-        Incidents incidents =
-            IncidentsConvert.convertDataTransferObjetToEntity(incidentsDTO, organization);
+    Incidents incidents =
+        IncidentsConvert.convertDataTransferObjetToEntity(incidentsDTO, organization);
 
-        Assert.assertEquals(incidents.getDescription(), incidentsDTO.getDescription());
-        Assert.assertEquals(incidents.getTitle(), incidentsDTO.getTitle());
-    }
+    Assert.assertEquals(incidents.getDescription(), incidentsDTO.getDescription());
+    Assert.assertEquals(incidents.getTitle(), incidentsDTO.getTitle());
+  }
 
 }
