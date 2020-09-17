4f3afe86d22279fa403a739a059daaedf83cfa70
muriloalvesdev <murilo.alves@idtrust.com.br>
2020-09-17 20:46:01 -0300

    implementando lombok na ApiException

--- src/main/java/br/com/thehero/domain/model/ApiException.java
+++ src/main/java/br/com/thehero/domain/model/ApiException.java
@@ -1,29 +1,14 @@
 package br.com.thehero.domain.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
 public class ApiException {
   private String message;
   private int status;
-
-  public ApiException(String message, int status) {
-    this.message = message;
-    this.status = status;
-  }
-
-  public String getMessage() {
-    return message;
-  }
-
-  public void setMessage(String message) {
-    this.message = message;
-  }
-
-  public int getStatus() {
-    return status;
-  }
-
-  public void setStatus(int status) {
-    this.status = status;
-  }
-
 
 }
