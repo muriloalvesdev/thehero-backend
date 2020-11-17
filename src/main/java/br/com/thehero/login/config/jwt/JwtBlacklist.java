f5dc71bebe40cbec4a24ad415cc33e18a86cd183
Jonatas Araujo <jonatas.araujo@dartdigital.com.br>
2020-11-17 13:18:47 -0300

    refactor : otimizing concatenation logger

--- src/main/java/br/com/thehero/login/config/jwt/JwtBlacklist.java
+++ src/main/java/br/com/thehero/login/config/jwt/JwtBlacklist.java
@@ -1,21 +1,23 @@
 package br.com.thehero.login.config.jwt;
 
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jwts;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
-import org.apache.commons.lang3.StringUtils;
-import org.apache.log4j.Logger;
-import org.springframework.context.annotation.Scope;
-import org.springframework.stereotype.Component;
-import io.jsonwebtoken.ExpiredJwtException;
-import io.jsonwebtoken.Jwts;
 
 @Component
 @Scope("singleton")
 public class JwtBlacklist {
   private static Map<String, String> blacklist = new ConcurrentHashMap<>();
-  private static final Logger LOGGER = Logger.getLogger(JwtBlacklist.class);
+  private static final Logger LOGGER = LoggerFactory.getLogger(JwtBlacklist.class);
 
   private String jwtSecret;
 
@@ -23,7 +25,7 @@ public class JwtBlacklist {
     if (!blacklist.containsKey(token)) {
       blacklist.putIfAbsent(token, String.valueOf(System.currentTimeMillis()));
     } else {
-      LOGGER.info("TOKEN JA FOI INVALIDADO, TOKEN: " + token);
+      LOGGER.info("TOKEN JA FOI INVALIDADO, TOKEN: {}", token);
     }
   }
 
