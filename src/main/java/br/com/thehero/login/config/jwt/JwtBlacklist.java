0ad5db9e732800b86f683fdad2c7e545a7b2a25b
muriloalvesdev <muriloalvesdev.ti@gmail.com>
2020-11-17 17:18:09 -0300

    logs alterados nas classes referente a jwt

--- src/main/java/br/com/thehero/login/config/jwt/JwtBlacklist.java
+++ src/main/java/br/com/thehero/login/config/jwt/JwtBlacklist.java
@@ -5,25 +5,24 @@ import java.time.ZoneId;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import org.apache.commons.lang3.StringUtils;
-import org.apache.log4j.Logger;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jwts;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Component
 @Scope("singleton")
 public class JwtBlacklist {
   private static Map<String, String> blacklist = new ConcurrentHashMap<>();
-  private static final Logger LOG = Logger.getLogger(JwtBlacklist.class);
-
   private String jwtSecret;
 
   public synchronized void add(String token) {
     if (!blacklist.containsKey(token)) {
       blacklist.putIfAbsent(token, String.valueOf(System.currentTimeMillis()));
     } else {
-      LOG.info("TOKEN JA FOI INVALIDADO, TOKEN: " + token);
+      log.info("TOKEN JA FOI INVALIDADO, TOKEN: " + token);
     }
   }
 
@@ -47,13 +46,7 @@ public class JwtBlacklist {
   }
 
   private LocalDateTime getDateExpirationToken(String token) {
-    return Jwts.parser()
-        .setSigningKey(jwtSecret)
-        .parseClaimsJws(token)
-        .getBody()
-        .getExpiration()
-        .toInstant()
-        .atZone(ZoneId.systemDefault())
-        .toLocalDateTime();
+    return Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(token).getBody().getExpiration()
+        .toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime();
   }
 }
