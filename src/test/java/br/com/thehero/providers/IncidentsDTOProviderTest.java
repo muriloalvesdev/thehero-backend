aa5066815dcd784c8c3d1c0f2aaa11ff07737cb3
muriloalvesdev <muriloalvesdev.ti@gmail.com>
2020-11-28 16:18:41 -0300

    incidentsDTOProvider for tests

---
+++ src/test/java/br/com/thehero/providers/IncidentsDTOProviderTest.java
@@ -0,0 +1,22 @@
+package br.com.thehero.providers;
+
+import java.util.stream.Stream;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.ArgumentsProvider;
+import br.com.thehero.IncidentsConstantsTest;
+import br.com.thehero.OrganizationConstantsTest;
+import br.com.thehero.dto.IncidentsDTO;
+
+public class IncidentsDTOProviderTest
+    implements ArgumentsProvider, OrganizationConstantsTest, IncidentsConstantsTest {
+  @Override
+  public Stream<? extends Arguments> provideArguments(ExtensionContext context) throws Exception {
+    return Stream.of(IncidentsDTO.newBuilder().city(OrganizationConstantsTest.CITY)
+        .description(IncidentsConstantsTest.DESCRIPTION).email(OrganizationConstantsTest.EMAIL_ONG)
+        .mimeType(IncidentsConstantsTest.MIME_TYPE).nameOrganization(OrganizationConstantsTest.NAME)
+        .title(IncidentsConstantsTest.TITLE).value(IncidentsConstantsTest.VALUE)
+        .uf(OrganizationConstantsTest.UF).whatsapp(OrganizationConstantsTest.WHATSAPP).build())
+        .map(Arguments::of);
+  }
+}
