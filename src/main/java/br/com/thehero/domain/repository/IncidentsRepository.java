f12850408aa72983ca1130dd1ac291046103bc2d
muriloalvesdev <muriloalvesdev.ti@gmail.com>
2020-11-28 16:53:16 -0300

    fix

--- src/main/java/br/com/thehero/domain/repository/IncidentsRepository.java
+++ src/main/java/br/com/thehero/domain/repository/IncidentsRepository.java
@@ -1,7 +1,6 @@
 package br.com.thehero.domain.repository;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -11,10 +10,7 @@ import br.com.thehero.domain.model.Incidents.Status;
 import br.com.thehero.domain.model.Organization;
 
 public interface IncidentsRepository extends JpaRepository<Incidents, UUID> {
-
   List<Incidents> findByOrganization(Organization organization);
 
   Page<Incidents> findByStatus(Status status, Pageable pageable);
-
-  Optional<Incidents> findByIdAndOrganization(UUID uuid, Organization organization);
 }
