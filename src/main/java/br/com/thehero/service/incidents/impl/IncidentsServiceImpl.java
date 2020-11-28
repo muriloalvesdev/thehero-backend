4092934fdde673e7e895f60040d27d66a76e1bbe
muriloalvesdev <muriloalvesdev.ti@gmail.com>
2020-11-28 16:18:58 -0300

    improving

--- src/main/java/br/com/thehero/service/incidents/impl/IncidentsServiceImpl.java
+++ src/main/java/br/com/thehero/service/incidents/impl/IncidentsServiceImpl.java
@@ -10,6 +10,7 @@ import br.com.thehero.domain.model.Organization;
 import br.com.thehero.domain.repository.IncidentsRepository;
 import br.com.thehero.domain.repository.OrganizationRepository;
 import br.com.thehero.dto.IncidentsDTO;
+import br.com.thehero.exception.OrganizationNotFoundException;
 import br.com.thehero.service.convert.IncidentsConvert;
 import br.com.thehero.service.incidents.IncidentsService;
 import javassist.NotFoundException;
@@ -20,45 +21,40 @@ import lombok.AllArgsConstructor;
 @Service
 public class IncidentsServiceImpl implements IncidentsService {
 
+  static final String ORGANIZATION_NOT_FOUND = "Não existe uma organização com o CNPJ informado.";
+  static final String INCIDENT_NOT_FOUND = "Não existe um incidente com o ID informado";
+  static final String UNAUTHORIZED = "Não autorizado!";
+  
   private IncidentsRepository incidentsRepository;
   private OrganizationRepository organizationRepository;
 
   public Incidents create(IncidentsDTO incidentsDTO, String cnpjOrganization) {
-    Organization organization =
-        this.organizationRepository
-            .findByCnpj(cnpjOrganization)
-            .orElseThrow(
-                () -> new RuntimeException("Não existe uma organização com o CNPJ informado."));
+    Organization organization = this.organizationRepository.findByCnpj(cnpjOrganization)
+        .orElseThrow(() -> new OrganizationNotFoundException(ORGANIZATION_NOT_FOUND));
     Incidents incidents =
         IncidentsConvert.convertDataTransferObjetToEntity(incidentsDTO, organization);
     return this.incidentsRepository.saveAndFlush(incidents);
   }
 
   public Page<IncidentsDTO> findAll(Pageable pageable) {
-    return this.incidentsRepository
-        .findByStatus(Status.AVAILABLE, pageable)
+    return this.incidentsRepository.findByStatus(Status.AVAILABLE, pageable)
         .map(IncidentsConvert::convertEntityToDataTransferObject);
   }
 
   public void delete(String incidentId, String cnpjOrganization) {
-    this.incidentsRepository
-        .findById(UUID.fromString(incidentId))
-        .ifPresent(
-            incident -> {
-              if (incident.getOrganization().getCnpj().equals(cnpjOrganization)) {
-                incident.setStatus(Status.NOT_AVAILABLE);
-                this.incidentsRepository.save(incident);
-              } else {
-                throw new IllegalAccessError("Não autorizado!");
-              }
-            });
+    this.incidentsRepository.findById(UUID.fromString(incidentId)).ifPresent(incident -> {
+      if (incident.getOrganization().getCnpj().equals(cnpjOrganization)) {
+        incident.setStatus(Status.NOT_AVAILABLE);
+        this.incidentsRepository.save(incident);
+      } else {
+        throw new IllegalAccessError(UNAUTHORIZED);
+      }
+    });
   }
 
   public IncidentsDTO findById(String incidentId) throws NotFoundException {
-    return this.incidentsRepository
-        .findById(UUID.fromString(incidentId))
-        .filter(Incidents::isAvailable)
-        .map(IncidentsConvert::convertEntityToDataTransferObject)
-        .orElseThrow(() -> new NotFoundException("Não existe um incidente com o ID informado"));
+    return this.incidentsRepository.findById(UUID.fromString(incidentId))
+        .filter(Incidents::isAvailable).map(IncidentsConvert::convertEntityToDataTransferObject)
+        .orElseThrow(() -> new NotFoundException(INCIDENT_NOT_FOUND));
   }
 }
