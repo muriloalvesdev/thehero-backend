d3c97f362c0d3da5760019283ec547d47b32846d
muriloalvesdev <muriloalvesdev.ti@gmail.com>
2020-12-28 00:12:13 -0300

    add more one test for context update organization

--- src/test/java/br/com/thehero/service/organization/impl/TestOrganizationServiceImpl.java
+++ src/test/java/br/com/thehero/service/organization/impl/TestOrganizationServiceImpl.java
@@ -3,6 +3,7 @@ package br.com.thehero.service.organization.impl;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import java.util.Optional;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ArgumentsSource;
@@ -12,6 +13,7 @@ import br.com.thehero.domain.model.Organization;
 import br.com.thehero.domain.repository.OrganizationRepository;
 import br.com.thehero.dto.OrganizationDTO;
 import br.com.thehero.providers.OrganizationDTOProviderTest;
+import br.com.thehero.service.convert.OrganizationConvert;
 import br.com.thehero.service.organization.OrganizationService;
 
 class TestOrganizationServiceImpl {
@@ -36,5 +38,22 @@ class TestOrganizationServiceImpl {
     this.service.create(dto);
 
     verify(this.repository, times(1)).save(Mockito.any());
+  }
+
+  @ParameterizedTest
+  @ArgumentsSource(OrganizationDTOProviderTest.class)
+  void shouldUpdateWithSuccess(OrganizationDTO dto) {
+    // given
+    Organization organization = OrganizationConvert.convertDataTransferObjectToEntity(dto);
+
+    BDDMockito.when(this.repository.findByCnpj(dto.getCnpj()))
+        .thenReturn(Optional.of(organization));
+    BDDMockito.when(this.repository.saveAndFlush(organization)).thenReturn(organization);
+
+    // then
+    this.service.update(dto);
+
+    verify(this.repository, times(1)).findByCnpj(Mockito.anyString());
+    verify(this.repository, times(1)).saveAndFlush(Mockito.any());
   }
 }
